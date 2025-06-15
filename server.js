77bac3ff68dc049c8d4d174115b49ffa34acd074
Marish <marishalagiri@gmail.com>
2025-06-16 00:21:58 +0530

    PORT

--- server.js
+++ server.js
@@ -19,11 +19,12 @@ app.use('/api/job', require('./routes/jobRoute'));
 app.use('/api/applyjob', require('./routes/applyJobRoute'));
 app.use('/api/delete', require('./routes/deleteRoute'));
 
-// MongoDB connection
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
+ 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 mongoose.connect(process.env.MONGO_URI)
 
